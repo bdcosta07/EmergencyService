9f96d77b1320deafe9f6e65e70a10f6a7d46b13f
Ratul <ratul.bside@outlook.com>
2016-07-20 01:49:28 +0600

    Some more edit with material

--- app/src/main/java/com/kichukkhon/emergencyservice/Activity/MainActivity.java
+++ app/src/main/java/com/kichukkhon/emergencyservice/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.kichukkhon.emergencyservice.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,11 +13,17 @@ import com.kichukkhon.emergencyservice.R;
 public class MainActivity extends ESBaseActivity {
     ImageView imgAmbulance, imgFireService, imgPolice;
     SharedPreference preference;
+    private Toolbar mToolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         imgAmbulance=(ImageView)findViewById(R.id.imgAmbulance);
         imgFireService=(ImageView)findViewById(R.id.imgFire);
