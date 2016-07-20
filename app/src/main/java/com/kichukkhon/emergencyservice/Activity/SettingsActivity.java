de683cfed2d9213b5cf63cfbc107faa974c2de75
Ratul <ratul.bside@outlook.com>
2016-07-20 14:28:56 +0600

    Main settings screen

--- app/src/main/java/com/kichukkhon/emergencyservice/Activity/SettingsActivity.java
+++ app/src/main/java/com/kichukkhon/emergencyservice/Activity/SettingsActivity.java
@@ -1,9 +1,23 @@
 package com.kichukkhon.emergencyservice.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Build;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.kichukkhon.emergencyservice.Class.Areas;
@@ -13,26 +27,104 @@ import com.kichukkhon.emergencyservice.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
     Spinner spinnerArea;
-    AreaManager manager;
+    AreaManager areaManager;
     List<Areas> areaList;
+    private static String appVersion;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.preference);
 
-        spinnerArea=(Spinner)findViewById(R.id.spinnerArea);
-        areaList=new ArrayList<>();
-        manager=new AreaManager(SettingsActivity.this);
-        areaList= manager.getAllAreas();
+        ListPreference projectsList = (ListPreference)findPreference("preferred_area_id");
 
-        ArrayAdapter<Areas> dataAdapter = new ArrayAdapter<Areas>(this, android.R.layout.simple_spinner_item, areaList);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spinnerArea.setAdapter(dataAdapter);
+        areaManager = new AreaManager(this);
+        List<Areas> areaList = areaManager.getAllAreas();
+
+        List<String> entries = new ArrayList<String>();
+        for (Areas area:areaList) {
+            entries.add(area.getAreaName());
+        }
+        List<String> entryValues = new ArrayList<String>();
+        for (Areas area:areaList) {
+            entryValues.add(Integer.toString(area.getAreaId()));
+        }
+
+        final CharSequence[] entryCharSeq = entries.toArray(new CharSequence[entries.size()]);
+        final CharSequence[] entryValsChar = entryValues.toArray(new CharSequence[entryValues.size()]);
+
+
+        projectsList.setEntries(entryCharSeq);
+
+        projectsList.setEntryValues(entryValsChar);
     }
 
-    public void btnAreaSave(View view) {
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Toolbar bar;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+            root.addView(bar, 0); // insert at top
+        } else {
+            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+            ListView content = (ListView) root.getChildAt(0);
+
+            root.removeAllViews();
+
+            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            }else{
+                height = bar.getHeight();
+            }
+
+            content.setPadding(0, height, 0, 0);
+
+            root.addView(content);
+            root.addView(bar);
+        }
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
     }
 }
